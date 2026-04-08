33f1325b938658d96e24db7f49d0a45ae3d8eb37
ArryoRuma <lwtruman2@gmail.com>
2026-04-08 14:47:06 -0400

    persist

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/pinetarsportsfund/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8787',
-        changeOrigin: true,
-      },
-    },
-  },
 })
