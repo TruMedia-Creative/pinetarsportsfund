05795b832675dbe44dea1ae0eaa4c7315dffdf1c
ArryoRuma <lwtruman2@gmail.com>
2026-04-29 10:17:11 -0400

    header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,43 +1,3 @@
-<script setup lang="ts">
-const route = useRoute()
-const requestFetch = useRequestFetch()
-
-const { data: authStatus } = await useAsyncData('studio-auth-status', () =>
-  requestFetch('/api/__studio/auth/status')
-)
-
-const isStudioAuthenticated = computed(() => Boolean(authStatus.value?.authenticated))
-
-const items = computed(() => {
-  const baseItems = [
-    {
-      label: 'About',
-      to: '/#about'
-    },
-    {
-      label: 'Offerings',
-      to: '/investments',
-      active: route.path.startsWith('/investments')
-    },
-    {
-      label: 'Investment Opportunities',
-      to: '/investments',
-      active: route.path.startsWith('/investments')
-    }
-  ]
-
-  if (isStudioAuthenticated.value) {
-    baseItems.push({
-      label: 'Studio Guide',
-      to: '/studio-instructions',
-      active: route.path === '/studio-instructions'
-    })
-  }
-
-  return baseItems
-})
-</script>
-
 <template>
   <UHeader>
     <template #left>
@@ -49,50 +9,13 @@ const items = computed(() => {
       </NuxtLink>
     </template>
 
-    <UNavigationMenu
-      :items="items"
-      variant="link"
-    />
-
     <template #right>
-      <UColorModeButton class="hidden lg:flex" />
-      <UButton
-        label="Contact Us"
-        color="neutral"
-        variant="ghost"
-        class="hidden lg:flex"
-        to="mailto:info@pinetarsportsfund.com"
-      />
       <UButton
         label="View Investment Opportunities"
         color="primary"
-        class="hidden lg:flex"
         to="/investments"
       />
-    </template>
-
-    <template #body>
-      <UNavigationMenu
-        :items="items"
-        orientation="vertical"
-      />
-
-      <div class="mt-4 flex flex-col gap-2">
-        <UButton
-          label="Contact Us"
-          color="neutral"
-          variant="soft"
-          block
-          to="mailto:info@pinetarsportsfund.com"
-        />
-        <UButton
-          label="View Investment Opportunities"
-          color="primary"
-          block
-          to="/investments"
-        />
-        <UColorModeButton block />
-      </div>
+      <UColorModeButton />
     </template>
   </UHeader>
 </template>
